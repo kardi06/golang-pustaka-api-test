a35d96587a0c5bc6c62ef943697eda91f2514af4
kardi.ib <kardi.ib@gmail.com>
2025-06-19 22:47:19 +0700

    fix(main): change port and import http

--- main.go
+++ main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"name": "Bang Kar",
 			"bio":  "Software Engineer",
 		})
 	})
 
-	router.Run()
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"subtitle": "Hello World",
+			"title": "Belajar golang aja deh",
+		})
+	})
+
+	// router.Run()
+	router.Run(":8888")
 }
