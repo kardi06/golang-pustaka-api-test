c695ed3d7f9957b47bb7c8e35e18d8836829bc0e
kardi.ib <kardi.ib@gmail.com>
2025-06-19 22:51:17 +0700

    fix(main): add handler for routing

--- main.go
+++ main.go
@@ -8,20 +8,24 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "Bang Kar",
-			"bio":  "Software Engineer",
-		})
-	})
+	router.GET("/", rootHandler)
 
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"subtitle": "Hello World",
-			"title": "Belajar golang aja deh",
-		})
-	})
+	router.GET("/hello", helloHandler)
 
 	// router.Run()
 	router.Run(":8888")
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Bang Kar",
+		"bio":  "Software Engineer",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"subtitle": "Hello World",
+		"title": "Belajar golang aja deh",
+	})
 }
